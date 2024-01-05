aa4e509eae123eddac9dbf5044ee5b7f34e8cae0
enzoclock <enzo.testa@oclock.io>
2024-01-05 15:08:51 +0100

    docs: heritage

--- docs/sandbox/6.Vehicule.js
+++ docs/sandbox/6.Vehicule.js
@@ -28,4 +28,50 @@ class Vehicule {
   }
 }
 
-module.exports = { Vehicule };
+// Notion HERITAGE
+// - Car ETEND la classe Vehicule. 
+// - Car HERITE de la classe Vehicule
+
+class Car extends Vehicule {
+
+  bootCapacity; // Capacité du coffre
+
+  constructor(enginePower, bootCapacity) { // enginePower = 50
+    super(4, enginePower); // super() === Vehicule.constructor()
+    this.bootCapacity = bootCapacity;
+  }
+
+  openDoors() {
+    console.log("**Bruit de portes qui s'ouvrent**");
+  }
+
+  // ⚡️ 'CAR' hérite de toutes les propriétés de Vehicule !  (Pas besoin de les renoté en dessous !)
+}
+
+class Moto extends Vehicule {
+  constructor(enginePower) { // Toute classe necessite un constructeur
+    super(2, enginePower); // Toute classe qui hérite d'une autre doit appeler le parent !
+  }
+
+  // ⚡️ 'MOTO' hérite de toutes les METHODES de Vehicule ! (pas besoin de les réimplémenter)
+
+  doAWheelies() {
+    console.log("**Bruit de roue avant** couiiiiiiink");
+  }
+}
+
+class ElectricCar extends Car {
+  batteryCapacity; // en kwh
+
+  constructor(enginePower, bootCapacity, batteryCapacity) {
+    super(enginePower, bootCapacity);
+    this.batteryCapacity = batteryCapacity;
+  }
+
+  charge() {
+    console.log(`La voiture charge...`);
+  }
+}
+
+
+module.exports = { Vehicule, Car, Moto, ElectricCar };
--- docs/sandbox/6.main.js
+++ docs/sandbox/6.main.js
@@ -1,2 +1,16 @@
-const { Vehicule } = require("./6.Vehicule");
+const { Car, Moto, ElectricCar } = require("./6.Vehicule");
+
+const clio = new Car(50, 500);
+console.log(clio);
+clio.openDoors();
+
+
+const harley = new Moto(100);
+harley.start();
+harley.makeNoise();
+// harley.openDoors(); // Erreur harley.openDoors is not a function ==> normal, car la méthode openDoors, c'est pour les Car
+harley.doAWheelies();
+
+const suvElectric = new ElectricCar(150, 600, 3);
+suvElectric.charge();
 
